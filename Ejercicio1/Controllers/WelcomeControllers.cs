be33cd1383f215e43ed65ed8a5ac6bdf4274eba9
Walker01167 <guerrerowalker7@gmail.com>
2025-06-11 20:58:07 -0400

    agregando el primer ejercicio

---
+++ Ejercicio1/Controllers/WelcomeControllers.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ejercicio1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WelcomeControllers : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get ([FromQuery] string name)
+        {
+            var mensaje = new { mensaje = $"¡Bienvenido señor, {name}!" };
+            return Ok(mensaje);
+        }
+    }
+}
