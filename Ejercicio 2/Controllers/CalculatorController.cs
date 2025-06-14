d5a0ec57cbf140f77d33327e93236901d27f65ec
Walker01167 <guerrerowalker7@gmail.com>
2025-06-13 22:41:20 -0400

    este es el ejercicio 2

---
+++ Ejercicio 2/Controllers/CalculatorController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ejercicio_2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet ("add")]
+        public IActionResult Add([FromQuery] double a, [FromQuery] double b)
+        {
+            return Ok(new { resultado = a + b });
+        }
+
+        [HttpGet("subtract")]
+        public IActionResult Subtract([FromQuery] double a, [FromQuery] double b)
+        {
+            return Ok (new { resultado = a - b });
+        }
+
+        [HttpGet("multiply")]
+        public IActionResult Multiply([FromQuery] double a, [FromQuery] double b)
+        {
+            return Ok(new { resultado = a * b });
+        }
+
+        [HttpGet("divide")]
+        public IActionResult Divide([FromQuery] double a, [FromQuery] double b)
+        {
+            if (b == 0)
+                return BadRequest(new { error = "No se puede dividir por cero." });
+
+            return Ok(new { resultado = a / b });
+        }
+    }
+}
+
+
+
+
+
