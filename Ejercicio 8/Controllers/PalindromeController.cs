f777d999ede17f2e99534ddf1deafbd51ba3158a
Walker01167 <guerrerowalker7@gmail.com>
2025-06-13 22:45:16 -0400

    esto es el ejercicio 8

---
+++ Ejercicio 8/Controllers/PalindromeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ejercicio_8.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PalindromeController : ControllerBase
+    {
+        [HttpGet("check")]
+        public IActionResult CheckPalindrome([FromQuery] string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return BadRequest(new { error = "El texto no puede estar vacío." });
+            }
+
+            string cleanText = text.Replace(" ", "").ToLower();
+            string reversed = new string(cleanText.Reverse().ToArray());
+
+            bool esPalindromo = cleanText == reversed;
+
+            return Ok(new
+            {
+                texto = text,
+                esPalindromo
+            });
+        }
+    }
+}
