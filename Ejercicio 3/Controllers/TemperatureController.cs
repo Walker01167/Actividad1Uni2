1d76a318d72b45833edd1770f637cd05ada015ff
Walker01167 <guerrerowalker7@gmail.com>
2025-06-13 22:42:24 -0400

    este es el ejercicio 3

---
+++ Ejercicio 3/Controllers/TemperatureController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ejercicio_3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TemperatureController : ControllerBase
+    {
+        [HttpGet("toFahrenheit")]
+        public IActionResult ToFahrenheit([FromQuery] double celsius)
+        {
+            double fahrenheit = (celsius * 9 / 5) + 32;
+            return Ok(new { resultados = fahrenheit });
+        }
+
+        [HttpGet("toCelsius")]
+        public IActionResult ToCelsius([FromQuery] double fahrenheit)
+        {
+            double celsius = (fahrenheit - 32) * 5 / 9;
+            return Ok(new { resultados = celsius });
+        }
+    }
+}
